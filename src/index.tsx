985e9f708d88cd74c787192ace28cab3fe599a29
leonardomso <leonardomso11@gmail.com>
2019-06-22 13:55:49 -0300

    Creating the main file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,57 @@
-import * as React from 'react';
+import { useState, useEffect, useRef } from "react";
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+interface IParameters {
+  initialValues: any;
+  onSubmit: (values: any) => any;
+}
+
+const useSlatt = ({ initialValues, onSubmit }: IParameters) => {
+  const [values, setValues] = useState(initialValues || {});
+  const [errors, setErrors] = useState({});
+  const [touched, setTouched] = useState({});
+  const [onSubmitting, setOnSubmitting] = useState<boolean>(false);
+  const [onBlur, setOnBlur] = useState<boolean>(false);
+
+  const formRendered = useRef(true);
+
+  useEffect(() => {
+    if (!formRendered.current) {
+      setValues(initialValues);
+      setErrors({});
+      setTouched({});
+      setOnSubmitting(false);
+      setOnBlur(false);
+    }
+    formRendered.current = false;
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+    const { name, value } = target;
+    event.persist();
+    setValues((values: any) => ({ ...values, [name]: value }));
+  };
+
+  const handleBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+    const { name } = target;
+    setTouched({ ...touched, [name]: true });
+    setErrors({ ...errors });
+  };
+
+  const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (event) event.preventDefault();
+    setErrors({ ...errors });
+    onSubmit({ values });
+  };
+
+  return {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleSubmit
+  };
 };
+
+export default useSlatt;
