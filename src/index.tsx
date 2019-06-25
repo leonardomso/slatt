61fec1d353e2e19dd33c675f7f8d031922725d4c
leonardomso <leonardomso11@gmail.com>
2019-06-25 19:35:46 -0300

    Working, missing some things

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import { useState, useEffect, useRef } from "react";
 
-interface IParameters {
+interface UseSlattParameters {
   initialValues: any;
-  onSubmit: (values: any) => any;
+  onSubmit: any;
+  validate?: (values: any) => any;
 }
 
-const useSlatt = ({ initialValues, onSubmit }: IParameters) => {
+const useSlatt = ({
+  initialValues,
+  onSubmit,
+  validate
+}: UseSlattParameters) => {
   const [values, setValues] = useState(initialValues || {});
   const [errors, setErrors] = useState({});
   const [touched, setTouched] = useState({});
@@ -23,13 +28,13 @@ const useSlatt = ({ initialValues, onSubmit }: IParameters) => {
       setOnBlur(false);
     }
     formRendered.current = false;
-  }, []);
+  }, [initialValues]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { target } = event;
     const { name, value } = target;
     event.persist();
-    setValues((values: any) => ({ ...values, [name]: value }));
+    setValues({ ...values, [name]: value });
   };
 
   const handleBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,10 +44,10 @@ const useSlatt = ({ initialValues, onSubmit }: IParameters) => {
     setErrors({ ...errors });
   };
 
-  const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleSubmit = (event: any) => {
     if (event) event.preventDefault();
     setErrors({ ...errors });
-    onSubmit({ values });
+    onSubmit({ values, errors });
   };
 
   return {
@@ -50,6 +55,7 @@ const useSlatt = ({ initialValues, onSubmit }: IParameters) => {
     errors,
     touched,
     handleChange,
+    handleBlur,
     handleSubmit
   };
 };
